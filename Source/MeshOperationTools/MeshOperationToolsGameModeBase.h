5a4157bf5e7681c07308ee0292e53e5a96644051
NUZEROVI <nuvmcdxliv@gmail.com>
2023-01-30 11:33:43 +0800

    Create a new project in UE4.26 (C++ Template)

---
+++ Source/MeshOperationTools/MeshOperationToolsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeshOperationToolsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MESHOPERATIONTOOLS_API AMeshOperationToolsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
